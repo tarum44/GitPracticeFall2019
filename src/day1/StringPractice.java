af036c17858fa651f4934f088cf94a7bc182977b
muratkilinc <tarum44@gmail.com>
2020-02-29 14:59:31 -0600

    Added reversing

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -5,5 +5,12 @@ public class StringPractice {
         System.out.println("Hello World");
         String str="Java is fun";
         System.out.println(str);
+
+        char [] chars=str.toCharArray();
+        String reversestr="";
+        for(int i=chars.length-1;i>=0;i--){
+            reversestr += chars[i];
+        }
+        System.out.println("reversestr = " + reversestr);
     }
 }
